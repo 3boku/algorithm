d2d55d984c7983effa72da555277e6432182d117
정삼복 <68502584+3boku@users.noreply.github.com>
2024-02-18 15:55:58 +0900

    study1

---
+++ go/minus.go
@@ -0,0 +1,10 @@
+package main
+
+//func solution(num1 int, num2 int) int {
+//
+//	return num1 - num2
+//}
+//
+//func main() {
+//	fmt.Println(solution(2, 3))
+//}
---
+++ go/slash.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func solution(num1 int, num2 int) float64 {
+	result := num1 / num2
+	return
+}
+
+func main() {
+	fmt.Println(solution(3, 2))
+}
