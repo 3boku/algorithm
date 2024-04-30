b8d75ae6d1d7f17cd0574cde3762194011f73030
정삼복 <68502584+3boku@users.noreply.github.com>
2024-04-30 15:17:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 37.98 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,14 @@
+function solution(s)
+{
+    const stack = [];
+    
+    for(let char of s){
+        if(s.length > 0 && stack[stack.length - 1] === char){
+            stack.pop();
+        } else{
+            stack.push(char);
+        }
+    }
+    
+    return stack.length === 0 ? 1 : 0
+}
